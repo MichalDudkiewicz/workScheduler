1fbb0ee156ce2862797eb88e4d71ef8d62aa64bc
Julaszym1212 <student@student-iit>
2020-01-02 15:52:49 +0100

    ScheduleTest global variables

--- trunk/project/library/test/scheduleTest.cpp
+++ trunk/project/library/test/scheduleTest.cpp
@@ -2,19 +2,28 @@
 #include "schedule.h"
 #include <memory>
 
-
 BOOST_AUTO_TEST_SUITE(TestSuiteCorrect)
+    boost::gregorian::date d(boost::gregorian::day_clock::local_day());
+    boost::gregorian::date startDate(d.year(),d.month()+1,1);
+    int day = startDate.day_of_week()-1;
 
-    BOOST_AUTO_TEST_CASE(ScheduleCase)
+    BOOST_AUTO_TEST_CASE(ScheduleCaseGetCurrentDate)
     {
-        boost::gregorian::date d(boost::gregorian::day_clock::local_day());
         BOOST_CHECK_EQUAL(Schedule::getCurrentDate(),d);
-        d += boost::gregorian::months(1);
-        BOOST_CHECK_EQUAL(Schedule::getNumberOfDays(),boost::gregorian::gregorian_calendar::end_of_month_day(d.year(),d.month()));
-        boost::gregorian::date startDate(d.year(),d.month(),1);
-        BOOST_CHECK_EQUAL(Schedule::getStartDate(),startDate);
-        int day = startDate.day_of_week()-1;
-        BOOST_CHECK_EQUAL(Schedule::getWeekDayIterator(startDate),day);
     }
 
+    BOOST_AUTO_TEST_CASE(ScheduleCaseGetNumberOfDays)
+    {
+        d += boost::gregorian::months(1);
+        BOOST_CHECK_EQUAL(Schedule::getNumberOfDays(),boost::gregorian::gregorian_calendar::end_of_month_day(d.year(),d.month()));
+    }
+
+    BOOST_AUTO_TEST_CASE(ScheduleCaseStartDate)
+    {
+        BOOST_CHECK_EQUAL(Schedule::getStartDate(),startDate);
+    }
+    BOOST_AUTO_TEST_CASE(ScheduleCaseGetWeekDayIterator)
+    {
+        BOOST_CHECK_EQUAL(Schedule::getWeekDayIterator(startDate),day);
+    }
 BOOST_AUTO_TEST_SUITE_END()
