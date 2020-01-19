51fd08641611064cb35017366e4870f92d85a2ed
Michał Dudkiewicz <michal.dudkiewicz96@gmail.com>
2020-01-19 23:04:42 +0100

    workScheduler Test

--- trunk/project/library/test/workSchedulerTest.cpp
+++ trunk/project/library/test/workSchedulerTest.cpp
@@ -1,14 +1,24 @@
 #include <boost/test/unit_test.hpp>
 #include "workScheduler.h"
 #include "employeeRepository.h"
+#include <iostream>
+#include "boost/date_time/gregorian/gregorian.hpp"
+#include "schedule.h"
+#include "employee.h"
+#include "shift.h"
 
 BOOST_AUTO_TEST_SUITE(TestSuiteCorrect)
 
     BOOST_AUTO_TEST_CASE(WorkSchedulerConstructionCase) {
+        boost::gregorian::date firstMonday(Schedule::getStartDate());
+        while(firstMonday.day_of_week()!=1)
+        {
+            firstMonday += boost::gregorian::days(1);
+        }
         WorkScheduler::getInstance().createSchedule();
         BOOST_TEST_MESSAGE(WorkScheduler::getInstance().scheduleInfo());
-        WorkScheduler::getInstance().updateSchedule();
-        BOOST_TEST_MESSAGE(WorkScheduler::getInstance().scheduleInfo());
+        BOOST_CHECK_EQUAL(WorkScheduler::getInstance().getSchedule()[0][0][0].empty(),true);
+        BOOST_CHECK_EQUAL(WorkScheduler::getInstance().getSchedule()[firstMonday.day()-1][0][0].front()->getId(),3);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
