417eece4c3a404f815f9623ca843f29caca4515e
Michal Dudkiewicz <michal.dudkiewicz96@gmail.com>
2020-02-17 23:59:40 +0100

    mySQL database connection established

--- trunk/project/program/src/main.cpp
+++ trunk/project/program/src/main.cpp
@@ -1,49 +1,50 @@
-#include <iostream>
 #include "managers/workScheduler.h"
-#include "file_stream/fileStream.h"
-#include <chrono>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
 
 using namespace std;
 
 int main() {
-    auto start = chrono::steady_clock::now();
-    input::loadPositions();
+    cout << endl;
+    cout << "Running 'SELECT 'Hello World!' AS _message'..." << endl;
+
     try {
-        //INPUT CSV FILES
-        input::teamRepository("../../../dataset/input/admin/teamRepository.csv");
-        input::teamSchedule("../../../dataset/input/admin/teamSchedule.csv");
-        input::employeeRepository("../../../dataset/input/admin/employeeRepository.csv");
-        input::desiredSchedule("../../../dataset/input/employees/desiredSchedules.csv");
+        sql::Driver *driver;
+        sql::Connection *con;
+        sql::Statement *stmt;
+        sql::ResultSet *res;
 
-        //CREATING AND DISPLAYING SCHEDULE
-        WorkScheduler::getInstance().createSchedule();
-        cout << WorkScheduler::getInstance().scheduleInfo();
+        /* Create a connection */
+        driver = get_driver_instance();
+        con = driver->connect("tcp://127.0.0.1:3306", "Michal", "1234");
+        /* Connect to the MySQL test database */
+        con->setSchema("work_scheduler");
 
-        //SAVING OUTPUT FILES
-        output::employeeRepository("../../../dataset/input/admin/employeeRepository.csv");
-        output::desiredSchedule("../../../dataset/input/employees/desiredSchedules.csv");
-        output::schedule("../../../dataset/output/schedule.csv");
-        output::teamRepository("../../../dataset/input/admin/teamRepository.csv");
-        output::teamSchedule("../../../dataset/input/admin/teamSchedule.csv");
-    }catch(FileException &error){
-        cout<<error.message();
+        stmt = con->createStatement();
+        res = stmt->executeQuery("SELECT 'Hello World!' AS _message");
+        while (res->next()) {
+            cout << "\t... MySQL replies: ";
+            /* Access column data by alias or column name */
+            cout << res->getString("_message") << endl;
+            cout << "\t... MySQL says it again: ";
+            /* Access column data by numeric offset, 1 is the first column */
+            cout << res->getString(1) << endl;
+        }
+        delete res;
+        delete stmt;
+        delete con;
+
+    } catch (sql::SQLException &e) {
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
     }
-    auto end = chrono::steady_clock::now();
 
-    cout << "Elapsed time in nanoseconds : "
-         << chrono::duration_cast<chrono::nanoseconds>(end - start).count()
-         << " ns" << endl;
+    cout << endl;
 
-    cout << "Elapsed time in microseconds : "
-         << chrono::duration_cast<chrono::microseconds>(end - start).count()
-         << " µs" << endl;
-
-    cout << "Elapsed time in milliseconds : "
-         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
-         << " ms" << endl;
-
-    cout << "Elapsed time in seconds : "
-         << chrono::duration_cast<chrono::seconds>(end - start).count()
-         << " sec";
     return 0;
 }
